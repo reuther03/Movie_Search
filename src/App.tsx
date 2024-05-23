f26c117e0ad287dac1f887d180dd62c43974af48
michaldrozd <michaldrozd03@gmail.com>
2024-05-23 18:09:07 +0200

    Added featching api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import './App.css'
+
+import {useEffect} from "react";
+
+//key b43c7385
+const API_URL = 'http://www.omdbapi.com?apikey=b43c7385'
 
 function App() {
 
+    const searchMovies = async (title:string) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json()
 
-  return (
-    <>
-    </>
-  )
+        console.log(data.Search)
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, [])
+
+    return (
+        <>
+        </>
+    )
 }
 
 export default App
