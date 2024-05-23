fd2f2ac23ce16fccd2786919a470dd2279940653
michaldrozd <michaldrozd03@gmail.com>
2024-05-24 00:56:00 +0200

    Added MovieCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import {useEffect, useState} from "react";
+import MovieCard from "./MovieCard.tsx";
+import "./App.css"
+import SearchIcon from './search.svg'
 
-import {useEffect} from "react";
-
-//key b43c7385
 const API_URL = 'http://www.omdbapi.com?apikey=b43c7385'
 
 function App() {
-
-    const searchMovies = async (title:string) => {
+    const [movies, setMovies] = useState([])
+    const [searchTerm, setSearchTerm] = useState("")
+    const searchMovies = async (title: string) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json()
 
+        setMovies(data.Search)
         console.log(data.Search)
     }
 
@@ -18,8 +21,34 @@ function App() {
     }, [])
 
     return (
-        <>
-        </>
+        <div className="app">
+            <h1>MovieSearch</h1>
+            <div className="search">
+                <input type="text"
+                       placeholder="Search"
+                       value={searchTerm}
+                       onChange={(e) => setSearchTerm(e.target.value) }
+                />
+                <img
+                    src={SearchIcon}
+                    alt="Search icon"
+                    onClick={() => searchMovies(searchTerm) }
+                />
+            </div>
+
+            {movies?.length > 0
+                ? (
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie}/>
+                        ))}
+                    </div>
+                ) : (
+                    <div className="empty">
+                        <h2>no movies found</h2>
+                    </div>
+                )}
+        </div>
     )
 }
 
---
+++ src/MovieProps.ts
@@ -0,0 +1,7 @@
+﻿// eslint-disable-next-line @typescript-eslint/no-unused-vars
+interface MovieProps {
+    Title: string
+    Year: string
+    Type: string
+    Poster: string
+}
